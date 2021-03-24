bdb3b97e2560a7ac2b328aeac6167f27cd694d04
Shaif Azad <shaif.rahi@northsouth.edu>
2021-03-24 23:57:47 +0600

    first commit

---
+++ scripts/ebay-scraper.js
@@ -0,0 +1,229 @@
+console.log('hello from scraper')
+const get_images_structure2=()=>{
+    let images={};
+    if(document.getElementsByClassName('cc-image')[0]){
+        images['thumbnail']=document.getElementsByClassName(' cc-image')[0].src;
+    }
+    if(document.getElementsByClassName('vi-image-gallery__image vi-image-gallery__image--absolute-center')[0]){
+        images['image']=document.getElementsByClassName('vi-image-gallery__image vi-image-gallery__image--absolute-center')[0].src;
+    }
+    return images;
+}
+const get_product_details=()=>{
+    //let details_section=document.getElementById('ProductDetails');
+    let details={};
+    let name_cells=document.getElementsByClassName('s-name');
+    let value_cells=document.getElementsByClassName('s-value');
+    for(let index=0;index<name_cells.length;index++){
+        details[name_cells[index].textContent]=value_cells[index].textContent;
+    }
+    return details;
+}
+function get_description(){
+    let description={};
+    let section;
+    if(document.getElementsByClassName('prodDetailSec')[0]!==undefined){
+        section=document.getElementsByClassName('prodDetailSec')[0];
+    }
+    else{
+        section=document.getElementsByClassName('section')[0]
+    }
+    let table;
+    section.childNodes.forEach((child)=>{
+        if(child.nodeName==="TABLE"){
+            table=child;
+        }
+    })
+    let rows=table.rows;
+    rows.forEach((row)=>{
+        if(row.cells.length>0){
+            let span1=document.getElementsByClassName("product-details-title")[0];
+            let span2=document.getElementsByClassName("product-details-title")[1];
+            let span3=document.getElementsByClassName("product-details-title")[2];
+            if(row.cells[0].innerText!==`<br class="br">` && !row.cells[0].contains(span1)&& !row.cells[0].contains(span2)&& !row.cells[0].contains(span3)){
+                description[row.cells[0].innerText]=row.cells[1].innerText;
+            }
+        }
+    })
+    return {description:description,table:table.outerHTML};
+}
+function set_color_variant_images(images,options){
+    let new_options_array=new Array()
+    for(let index=0;index<options.length;index++){
+        let variant={color:options[index],image:images[index+1]}
+        variant.image=images[index+1]
+        new_options_array.push(variant)
+    }
+    return new_options_array
+}
+function get_variants(){
+    let variants={};
+    if(document.getElementsByClassName('nonActPanel ').length===1){
+        if(document.getElementById('msku-sel-1')!==null){
+            let options=[];
+            document.getElementById('msku-sel-1').childNodes.forEach(child=>{
+                if(child.innerText!=="- Select -"){
+                    options.push(child.innerText)
+                }
+            })
+            variants[document.getElementById('msku-sel-1').getAttribute('name')]=options;
+        }
+        if(document.getElementById('msku-sel-2')!==null){
+            let options=[];
+            let images=get_images()
+            console.log('images',images)
+            document.getElementById('msku-sel-2').childNodes.forEach(child=>{
+                if(child.innerText!=="- Select -"){
+                    options.push(child.innerText)
+                }
+            })
+            variants[document.getElementById('msku-sel-2').getAttribute('name')]=set_color_variant_images(images,options);
+        }
+        if(document.getElementById('msku-sel-3')!==null){
+            let options=[];
+            document.getElementById('msku-sel-3').childNodes.forEach(child=>{
+                if(child.innerText!=="- Select -"){
+                    options.push(child.innerText)
+                }
+            })
+            variants[document.getElementById('msku-sel-3').getAttribute('name')]=options;
+        }
+        if(document.getElementById('msku-sel-4')!==null){
+            let options=[];
+            document.getElementById('msku-sel-4').childNodes.forEach(child=>{
+                if(child.innerText!=="- Select -"){
+                    options.push(child.innerText)
+                }
+            })
+            variants[document.getElementById('msku-sel-4').getAttribute('name')]=options;
+        }
+        //console.log(variants);
+        return variants;
+    }
+    return variants;
+}
+function get_images(){
+    let images=[];
+    if(document.querySelector('#vi_main_img_fs')!==null){
+        let tableDatas=document.querySelector('#vi_main_img_fs');
+        if(tableDatas.hasChildNodes()){
+            if(tableDatas.children[0].nodeName==="UL" && tableDatas.children[0].getAttribute('class')==='lst icon'){
+                tableDatas.children[0].childNodes.forEach((td)=>{
+                    if(td.hasChildNodes){
+                        td.childNodes.forEach((item)=>{
+                            if(item.hasChildNodes){
+                                item.childNodes.forEach((item)=>{ //table
+                                    if(item.hasChildNodes){
+                                        item.childNodes.forEach((item)=>{ //tbody
+                                            if(item.hasChildNodes){
+                                                item.childNodes.forEach((item)=>{
+                                                    if(item.hasChildNodes){
+                                                        item.childNodes.forEach(item=>{ //td
+                                                            if(item.hasChildNodes){
+                                                                item.childNodes.forEach(item=>{
+                                                                    if(item.hasChildNodes){
+                                                                        item.childNodes.forEach(img=>{
+                                                                            if(img.src!==undefined){
+                                                                                images.push(img.src);
+                                                                            }
+                                                                        })
+                                                                    }
+                                                                })
+                                                            }
+                                                        })
+                                                    }
+                                                })
+                                            }
+                                        })
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    }
+    return images;
+}
+function get_seller_info(){
+    let seller={};
+    if(document.getElementsByClassName('mbg-nw')!==null){
+        let spans=document.getElementsByClassName('mbg-nw');
+        spans.forEach(span=>{
+            if(span.parentNode.parentNode.className==="mbg vi-VR-margBtm3" && span.parentNode.parentNode.parentNode.className==="bdg-90"){
+                seller.name=span.innerText;
+                seller.profile=span.parentNode.getAttribute('href');
+            }
+        })
+    }
+    return seller;
+}
+function get_price(text){
+    let textContent=new String(text);
+    let price=textContent.split(" ");
+    price[1]=price[1].replace('$'," ").trim();
+    let currency=price[0]+" $";
+    return {price:price[1],currency:currency};
+}
+function get_item_id(){
+    let item_id="";
+    let element=document.querySelector("div.social-widget.vi-share-widget-tc div.sw a")
+    item_id=element.getAttribute('data-itemid');
+    return item_id;
+}
+function set_data(){
+    let titleElement=document.getElementById("itemTitle");
+    let priceElement=document.getElementById("prcIsum");
+    let mainImage = document.getElementById("icImg");
+    data={market:"ebay",item_id:get_item_id(),url:location.href,seller:get_seller_info(),title:titleElement.textContent.replace("Details about  "," ").trim(),variants:get_variants(),current_price:parseInt(get_price(priceElement.textContent).price),currency:get_price(priceElement.textContent).currency, default_image:mainImage.src,images:get_images(),details:get_product_details(),description:get_description().description,specification:get_description().table,shipping_and_payment:get_shipping_and_payment_details()};
+    chrome.storage.local.set({product_details:data})
+    console.log("Captured JSON object: "+JSON.stringify(data));
+}
+function get_shipping_and_payment_details(){
+    let shipping={shipping_to:[],excludes:[]}
+    if(document.getElementById('sh-gsp-wrap')!==null){
+        console.log('shipping and payment details div found')
+        document.getElementById('sh-gsp-wrap').childNodes.forEach(child=>{
+            if(new String(child.innerText).includes('Shipping') && child.className==='sh-sLoc'){
+                shipping.shipping_to=new String(child.innerText).replace('Shipping to:','').trim().split(',')
+            }
+            else if(child.className==='sh-sLoc' && new String(child.innerText).includes('Excludes')){
+                shipping.shipping_to=new String(child.innerText).replace('Excludes:','').trim().split(',')
+            }
+        })
+    }
+    return shipping;
+}
+function get_parameters(){
+    let url=new URL(window.location.href);
+    console.log(url.href.split('/'))
+    return url.href.split('/')
+}
+if(get_parameters().length>0){
+    chrome.storage.local.get(['lines'],function(result){
+        if(result.lines!==undefined){
+            if(result.lines.length>0 && result.lines[0].product_url===window.location.href){
+                chrome.runtime.sendMessage({message:'add_to_cart'})
+            }
+        }
+        else{
+            get_shipping_and_payment_details()
+            if(document.getElementById("itemTitle")){
+                console.log('cat 1 item')
+                set_data();
+            }
+            else if(document.getElementsByClassName('product-title')[0]){
+                console.log('cat 2 item')
+                let title=document.getElementsByClassName('product-title')[0].textContent;
+                let priceElement=document.getElementById("prcIsum");
+                let mainImage = document.getElementById("icImg");
+                let price=document.getElementsByClassName('display-price')[0].textContent.replace('$','').trim();
+                data={market:'ebay',item_id:get_item_id(),url:location.href,seller:get_seller_info(),title:title,variants:get_variants(),price:price,current_price:parseInt(get_price(priceElement.textContent).price),currency:get_price(priceElement.textContent).currency,default_image:mainImage.src,images:get_images_structure2(),details:get_product_details(),description:get_description(),specification:get_description().table,shipping_and_payment:get_shipping_and_payment_details()};
+                console.log("Captured JSON object: "+JSON.stringify(data));
+                //chrome.storage.local.set({product_details:data})
+            }
+        }
+    })
+}
+
---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const puppeteer = require('puppeteer');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+var multer = require('multer')
+var forms = multer();
+const path = __dirname + '/app/views/';
+const app = express();
+app.use(express.static(path));
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(forms.array());
+app.use(express.urlencoded({ extended: true }));
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+// app.post('/product', function (req, res) {
+//   puppeteer.launch().then(async function (browser) {
+//     const page = await browser.newPage();
+//     await page.goto(req.body.product_url);
+//     // Targeting the DOM Nodes that contain the Digimon names
+//     const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function (digimons) {
+//       // Mapping each Digimon name to an array
+//       return digimons.map(function (digimon) {
+//         return digimon.innerText;
+//       });
+//     });
+//     // Closing the Puppeteer controlled headless browser
+//     await browser.close();
+//   })
+// })
+app.post('/product', function (req, res) {
+  // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
+  puppeteer.launch().then(async function (browser) {
+    const page = await browser.newPage();
+    console.log(req.body.url)
+    await page.goto(req.body.url, {
+      waitUntil: 'networkidle2',
+    });
+    //await page.pdf({ path: 'hn.pdf', format: 'a4' });
+    //await page.screenshot({ path: 'ss.png' });
+    // const digimonNames = await page.$('#itemTitle', function (tag) {
+    //   console.log(tag)
+    //   return tag.innerText
+    //   // Mapping each Digimon name to an array
+    //   // return tag.children.map(function (digimon) {
+    //   //   return digimon.innerText;
+    //   // });
+    // });
+    const header = JSON.parse(await page.evaluate(() => {
+      const element = document.getElementById('itemTitle');
+      const main_image=document.getElementById('icImg').getAttribute('src')
+      console.log(element);
+      //scraper()
+      return JSON.stringify({title:element.innerText,main_image:main_image});
+    }));
+    await browser.close();
+    res.send(header)
+    // Sending the Digimon names to Postman
+    //res.sendFile("emdad.png");
+    //res.send(JSON.stringify(digimonNames))
+  });
+});
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
